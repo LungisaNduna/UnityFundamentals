5770a198e0e9d7b9ffba05677df552adb3185720
Lungisa Nduna <39986192+LungisaNduna@users.noreply.github.com>
2019-09-26 16:23:11 +0200

    committing the first parts of the project

---
+++ Assets/Scripts/MouseManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class MouseManager : MonoBehaviour
+{
+    // what objects are clickable
+    public LayerMask clickableLayer;
+
+    // swap cursors per object
+    public Texture2D pointer;
+    public Texture2D target;
+    public Texture2D doorway;
+    public Texture2D combat;
+
+    public EventVector3 OnClickEnviroment;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 50, clickableLayer.value))
+        {
+            bool door = false;
+            if (hit.collider.gameObject.tag == "Doorway"){
+                Cursor.SetCursor(doorway, new Vector2(16,16), CursorMode.Auto);
+                door = true;
+            }
+            else{
+                Cursor.SetCursor(target, new Vector2(16,16), CursorMode.Auto);
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                OnClickEnviroment.Invoke(hit.point);
+            }
+        }
+        else
+        {
+            Cursor.SetCursor(pointer, new Vector2(0, 0), CursorMode.Auto);
+        }
+    }
+}
+
+[System.Serializable]
+public class EventVector3: UnityEvent<Vector3> { }
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    //private Animator anim;
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //anim = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
